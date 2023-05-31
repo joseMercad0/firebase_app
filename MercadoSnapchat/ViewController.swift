5f1e098a42c3f5d734f21b95bcf3849a50598009
joseMercad0 <jose.mercado.c@tecsup.edu.pe>
2023-05-31 08:18:17 -0500

    Initial Commit

---
+++ MercadoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MercadoSnapchat
+//
+//  Created by Mac 15 on 31/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
